87087fd2c236adaaebdfd6f108c8b84d26dcb0f7
Alessandro Montesi <alessandromontesi234@gmail.com>
2025-01-29 22:42:58 -0800

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,20 +3,20 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="flex items-center justify-between flex-wrap p-4 sticky mb-4 bg-cream-white">
+        <nav className="flex items-center justify-between flex-wrap p-4 sticky mb-4 bg-cream">
             <div className="flex items-center flex-shrink-0 mr-6 text-xl text-dark-green">
                 <Link href="/" className="font-semibold tracking-tight ">
                     Logo
                 </Link>
             </div>
             <div className="ml-auto flex space-x-10 mr-8 text-md">
-                <Link href="/postings" className=" hover:text-green-900 ease-linear duration-150 text-dark-green">
+                <Link href="/postings" className=" hover:text-green-900 font-semibold ease-linear duration-150 text-dark-green">
                     Postings
                 </Link>
-                <Link href="/login" className="text-dark-green hover:text-green-900 ease-linear duration-150">
+                <Link href="/login" className="text-dark-green font-semibold hover:text-green-900 ease-linear duration-150">
                     Login
                 </Link>
-                <Link href="/register" className="text-dark-green hover:text-green-900 ease-linear duration-150">
+                <Link href="/register" className="text-dark-green font-semibold hover:text-green-900 ease-linear duration-150">
                     Register
                 </Link>
             </div>
