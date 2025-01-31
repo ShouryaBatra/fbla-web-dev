5749f20b65a8c76cd8b0e03248dd390d8c15e4b2
Alessandro Montesi <alessandromontesi234@gmail.com>
2025-01-30 23:00:05 -0800

    navbar changes + footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,15 @@
 import Image from "next/image";
 import Navbar from "@/components/navbar";
-
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
    <main>
     <Navbar />
-    <div className="text-4xl">
-      <a href="/really-important-page">
-        Click Me!
-      </a>
+    <div className="text-4xl mb-96">
+      <h1>Home</h1>
     </div>
-
+    <Footer />
    </main>
   );
 }
