a9f88511b2fbb5f1dbec45b3d9f52812d969c0cd
Shourya Batra <51655190+ShouryaBatra@users.noreply.github.com>
2025-02-19 19:35:05 -0800

    navbar responsiveness with hamburger

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,6 +5,7 @@ import { getFirestore, doc, getDoc } from "firebase/firestore";
 import { app } from "../../config/firebase";
 import Image from "next/image";
 import logo from "../assets/logo.png";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const auth = getAuth(app);
 const db = getFirestore(app);
@@ -14,6 +15,7 @@ const Navbar = () => {
   const [isAdmin, setIsAdmin] = useState(false);
   const [isEmployer, setIsEmployer] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
@@ -69,72 +71,134 @@ const Navbar = () => {
             height={140}
             className="ml-[-20px]"
           />
-          <p className="font-semibold tracking-tight text-2xl">
-            <p className="ml-[-20px] ">Homestead Careers</p>
-          </p>
+          <div className="font-semibold tracking-tight text-2xl hidden md:block">
+            <p className="ml-[-20px]">Homestead Careers</p>
+          </div>
         </Link>
         <div className="ml-auto flex space-x-10 mr-8 text-lg text-cream">
-          <Link
-            href="/postings"
-            className="hover:text-green-600 font-semibold ease-linear duration-150"
-          >
-            <p className="transition duration-300 hover:text-cream-white hover:scale-[1.05]">
-              Postings
-            </p>
-          </Link>
-          {user ? (
-            <div>
-              <button
-                onClick={() => setMenuOpen(!menuOpen)}
-                className="hover:text-green-600 font-semibold ease-linear duration-150"
-              >
-                <p className="transition duration-300 hover:text-cream-white hover:scale-[1.05] ">
-                  {formatName(user.name)}
-                </p>
-              </button>
-              {menuOpen && (
-                <div className="absolute right-0 mt-2 w-36 bg-white rounded-md shadow-lg py-2">
-                  <Link
-                    href="/profile"
-                    className="block w-full text-left px-4 py-1 text-gray-700 hover:bg-gray-100"
-                  >
-                    Profile
-                  </Link>
-                  {(isEmployer || isAdmin) && (
-                    <Link
-                      href="/new-posting"
-                      className="block w-full text-left px-4 py-1 text-gray-700 hover:bg-gray-100"
-                    >
-                      Create Postings
-                    </Link>
-                  )}
-                  {isAdmin && (
-                    <Link
-                      href="/admin"
-                      className="block w-full text-left px-4 py-1 text-gray-700 hover:bg-gray-100"
-                    >
-                      Admin Panel
-                    </Link>
-                  )}
-                  <button
-                    onClick={handleLogout}
-                    className="block w-full text-left px-4 py-1 text-gray-700 hover:bg-gray-100"
-                  >
-                    Logout
-                  </button>
-                </div>
-              )}
-            </div>
-          ) : (
+          <div className="hidden md:flex space-x-10">
             <Link
-              href="/login"
-              className="font-semibold hover:text-green-600 ease-linear duration-150"
+              href="/postings"
+              className="hover:text-green-600 font-semibold ease-linear duration-150"
             >
               <p className="transition duration-300 hover:text-cream-white hover:scale-[1.05]">
-                Login
+                Postings
               </p>
             </Link>
-          )}
+            {user ? (
+              <div>
+                <button
+                  onClick={() => setMenuOpen(!menuOpen)}
+                  className="hover:text-green-600 font-semibold ease-linear duration-150"
+                >
+                  <p className="transition duration-300 hover:text-cream-white hover:scale-[1.05]">
+                    {formatName(user.name)}
+                  </p>
+                </button>
+                {menuOpen && (
+                  <div className="absolute right-0 mt-2 w-36 bg-white rounded-md shadow-lg py-2">
+                    <Link
+                      href="/profile"
+                      className="block w-full text-left px-4 py-1 text-gray-700 hover:bg-gray-100"
+                    >
+                      Profile
+                    </Link>
+                    {(isEmployer || isAdmin) && (
+                      <Link
+                        href="/new-posting"
+                        className="block w-full text-left px-4 py-1 text-gray-700 hover:bg-gray-100"
+                      >
+                        Create Postings
+                      </Link>
+                    )}
+                    {isAdmin && (
+                      <Link
+                        href="/admin"
+                        className="block w-full text-left px-4 py-1 text-gray-700 hover:bg-gray-100"
+                      >
+                        Admin Panel
+                      </Link>
+                    )}
+                    <button
+                      onClick={handleLogout}
+                      className="block w-full text-left px-4 py-1 text-gray-700 hover:bg-gray-100"
+                    >
+                      Logout
+                    </button>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <Link
+                href="/login"
+                className="font-semibold hover:text-green-600 ease-linear duration-150"
+              >
+                <p className="transition duration-300 hover:text-cream-white hover:scale-[1.05]">
+                  Login
+                </p>
+              </Link>
+            )}
+          </div>
+          <div className="md:hidden">
+            <button
+              onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+              className="text-cream font-semibold"
+            >
+              <div className="text-3xl">
+                <RxHamburgerMenu />
+              </div>
+            </button>
+            {mobileMenuOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2">
+                <Link
+                  href="/postings"
+                  className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+                >
+                  Postings
+                </Link>
+                {user && (
+                  <>
+                    <Link
+                      href="/profile"
+                      className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+                    >
+                      Profile
+                    </Link>
+                    {(isEmployer || isAdmin) && (
+                      <Link
+                        href="/new-posting"
+                        className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+                      >
+                        Create Postings
+                      </Link>
+                    )}
+                    {isAdmin && (
+                      <Link
+                        href="/admin"
+                        className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+                      >
+                        Admin Panel
+                      </Link>
+                    )}
+                    <button
+                      onClick={handleLogout}
+                      className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+                    >
+                      Logout
+                    </button>
+                  </>
+                )}
+                {!user && (
+                  <Link
+                    href="/login"
+                    className="block w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100"
+                  >
+                    Login
+                  </Link>
+                )}
+              </div>
+            )}
+          </div>
         </div>
       </nav>
     </div>
