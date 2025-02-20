fb27853e7170d1f0cb43d60a7add4f0e647f2196
Shourya Batra <51655190+ShouryaBatra@users.noreply.github.com>
2025-02-19 20:05:53 -0800

    change footer elements and responsiveness

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 
 const navigation = {
@@ -7,15 +6,8 @@ const navigation = {
     { name: "Get Started", href: "/" },
     { name: "Benefits", href: "/" },
   ],
-  apply: [
-    { name: "Careers", href: "/postings" },
-    { name: "Application", href: "/" },
-  ],
-  other: [{ name: "Other", href: "/" }],
-  legal: [
-    { name: "Privacy", href: "" },
-    { name: "Terms", href: "" },
-  ],
+  positions: [{ name: "Careers", href: "/postings" }],
+  apply: [{ name: "Application", href: "/postings" }],
 };
 
 export default function Footer() {
@@ -26,90 +18,78 @@ export default function Footer() {
     >
       <div className="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-20 opacity-100">
         <div className="xl:grid xl:grid-cols-3 xl:gap-8">
+          {/* Logo and Title */}
           <Link
             href="/"
-            className="space-x-3 sm:flex text-2xl rounded-md 
-                font-semibold cursor-pointer ease-linear duration-300"
+            className="space-x-3 sm:flex text-2xl rounded-md font-semibold cursor-pointer ease-linear duration-300"
           >
             <p className="mt-3 hidden md:block text-cream hover:text-gray-100">
               Homestead Careers
             </p>
           </Link>
-          <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <h3 className="text-md font-semibold leading-6 text-cream">
-                  About
-                </h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.about.map((item) => (
-                    <li key={item.name}>
-                      <Link
-                        href={item.href}
-                        className="text-md leading-6 text-cream-white hover:text-gray-100"
-                      >
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div>
-                <h3 className="text-md font-semibold leading-6 text-cream mt-10 md:mt-0">
-                  Apply
-                </h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.apply.map((item) => (
-                    <li key={item.name}>
-                      <Link
-                        href={item.href}
-                        className="text-md leading-6 text-cream-white hover:text-gray-100"
-                      >
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+
+          {/* Navigation Links */}
+          <div className="mt-16 grid grid-cols-2 gap-8 sm:grid-cols-2 xl:grid-cols-3 xl:col-span-2 xl:mt-0">
+            {/* About Section */}
+            <div>
+              <h3 className="text-md font-semibold leading-6 text-cream">
+                About
+              </h3>
+              <ul role="list" className="mt-6 space-y-4">
+                {navigation.about.map((item) => (
+                  <li key={item.name}>
+                    <Link
+                      href={item.href}
+                      className="text-md leading-6 text-cream-white hover:text-gray-100"
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </div>
-            <div className="md:grid md:grid-cols-2 md:gap-8">
-              <div>
-                <h3 className="text-md font-semibold leading-6 text-cream">
-                  Other
-                </h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.other.map((item) => (
-                    <li key={item.name}>
-                      <Link
-                        href={item.href}
-                        className="text-md leading-6 text-cream-white hover:text-gray-100"
-                      >
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-              <div className="mt-10 md:mt-0">
-                <h3 className="text-md font-semibold leading-6 text-cream">
-                  Legal
-                </h3>
-                <ul role="list" className="mt-6 space-y-4">
-                  {navigation.legal.map((item) => (
-                    <li key={item.name}>
-                      <Link
-                        href={item.href}
-                        className="text-md leading-6 text-cream-white hover:text-gray-100"
-                      >
-                        {item.name}
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
+
+            {/* Positions Section */}
+            <div>
+              <h3 className="text-md font-semibold leading-6 text-cream">
+                Positions
+              </h3>
+              <ul role="list" className="mt-6 space-y-4">
+                {navigation.positions.map((item) => (
+                  <li key={item.name}>
+                    <Link
+                      href={item.href}
+                      className="text-md leading-6 text-cream-white hover:text-gray-100"
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            {/* Apply Section - Moves to the bottom on mobile screens */}
+            <div className="col-span-2 sm:col-span-2 xl:col-span-1">
+              <h3 className="text-md font-semibold leading-6 text-cream">
+                Apply
+              </h3>
+              <ul role="list" className="mt-6 space-y-4">
+                {navigation.apply.map((item) => (
+                  <li key={item.name}>
+                    <Link
+                      href={item.href}
+                      className="text-md leading-6 text-cream-white hover:text-gray-100"
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
+
+        {/* Copyright Section */}
         <div className="mt-8 md:mt-12 border-t border-cream pt-8 md:flex md:items-center md:justify-between">
           <p className="mt-8 text-md leading-5 text-neutral-300 md:order-1 md:mt-0">
             &copy; {new Date().getFullYear()} Homestead Careers. All rights
