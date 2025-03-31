74f33b48aabc231427ea1dc8c5dd48423d9f84d6
Shourya Batra <51655190+ShouryaBatra@users.noreply.github.com>
2025-03-31 14:45:36 -0700

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,23 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCJ2FcGysJqIMuDQm6B6weFWWpbnfdTfXc",
-  authDomain: "fbla-web-dev.firebaseapp.com",
-  projectId: "fbla-web-dev",
-  storageBucket: "fbla-web-dev.firebasestorage.app",
-  messagingSenderId: "340345869369",
-  appId: "1:340345869369:web:b19739049dcc6d49d1e9e8",
-  measurementId: "G-K074DPRQZM",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
